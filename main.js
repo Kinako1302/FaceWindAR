37c3fde3c6993169d21eab59df5dcec8b93f7785
yuta kondo <yuta050207@gmail.com>
2025-06-13 09:40:16 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,22 @@ const { renderer, scene, camera } = mindarThree;
 
 const start = async () => {
   console.log("MindAR 起動中...");
-  const confettiTexture = new THREE.TextureLoader().load("confetti.png");
-  const material = new THREE.SpriteMaterial({ map: confettiTexture, transparent: true });
-  const sprite = new THREE.Sprite(material);
 
-  sprite.scale.set(1, 1, 1);
-  sprite.position.set(0.5, 0.2, 0);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const confettiTexture = new THREE.TextureLoader().load("confetti.png");
+  const geometry = new THREE.PlaneGeometry(1, 1);
+  const material = new THREE.MeshBasicMaterial({ map: confettiTexture, transparent: true });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  mesh.position.set(0.5, 0.2, 0);
 
   const anchor = mindarThree.addAnchor(1);
-  anchor.group.add(sprite);
+  anchor.group.add(mesh);
 
   const animate = () => {
-    sprite.position.x -= 0.01;
-    if (sprite.position.x < -0.5) sprite.position.x = 0.5;
+    mesh.position.x -= 0.01;
+    if (mesh.position.x < -0.5) mesh.position.x = 0.5;
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   };
